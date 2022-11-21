146c085d2ea8ca9673cb6b294741e530911c7dee
PurpleGhost <101989624+PurpleGhost12@users.noreply.github.com>
2022-11-21 17:04:29 +0300

    shellSort

--- puzur.py
+++ puzur.py
@@ -1,15 +1,15 @@
-from random import randint
- 
-N = 10
-a = []
-for i in range(N):
-    a.append(randint(1, 99))
-print(a)
- 
- 
-for i in range(N-1):
-    for j in range(N-i-1):
-        if a[j] > a[j+1]:
-            a[j], a[j+1] = a[j+1], a[j]
- 
-print(a)
+def shellSort(array):
+    n = len(array)
+    k = int(math.log2(n))
+    interval = 2*k -1
+    while interval > 0:
+        for i in range(interval, n):
+            temp = array[i]
+            j = i
+            while j >= interval and array[j - interval] > temp:
+                array[j] = array[j - interval]
+                j -= interval
+            array[j] = temp
+        k -= 1
+        interval = 2*k -1
+    return array
